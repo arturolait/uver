b1f88b2c0dcb2d13e5b9b7dc67bd6dcd0e0b6662
arturolait <99770158+arturolait@users.noreply.github.com>
2022-02-17 09:52:13 -0600

    Update settings.config.php

--- core/settings.config.php
+++ core/settings.config.php
@@ -3,7 +3,7 @@
  * Database Connection Settings
  */
 define('DB_HOST', 'localhost');
-define('DB_USER', 'administrador');
-define('DB_PASS', 'Uv_2022');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'curriculum_uv');
 define('DRIVER', 'mysql');
